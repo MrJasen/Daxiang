0ca8e81e1de24e308063ca7af24a2c151a969d57
17611592013@163.com <17611592013@163.com>
2019-03-29 20:59:22 +0800

    更新书本案例4-1~4-2

---
+++ book/4-1.py
@@ -0,0 +1,4 @@
+pizza = ['榴莲味','草莓味','奶油味']
+for eat in pizza:
+    print('I like '+ eat +'pizza')
+print('I very like pizza')
---
+++ book/4-2.py
@@ -0,0 +1,3 @@
+animal = ['蛇','狗','猪']
+for eat in animal:
+    print(eat)
